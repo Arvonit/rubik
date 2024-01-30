b2d2cf504ec6299dd769d1c9f2241fa71eab72e5
Arvind Kasiliya <arvind@kasiliya.com>
2024-01-30 18:16:23 -0500

    Remove /docs endpoint

--- next.config.js
+++ next.config.js
@@ -7,18 +7,6 @@ const nextConfig = {
         source: '/api/:path*',
         destination:
           process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api/:path*' : '/api/'
-      },
-      {
-        source: '/docs',
-        destination:
-          process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api/docs' : '/api/docs'
-      },
-      {
-        source: '/openapi.json',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://localhost:8000/api/openapi.json'
-            : '/api/openapi.json'
       }
     ];
   }
